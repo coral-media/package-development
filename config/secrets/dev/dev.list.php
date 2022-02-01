173596a01f9f6b1ec87957262e04423e3b8285f0
rernesto <rernesto.espinosa@gmail.com>
2022-02-01 14:16:42 -0500

    Initial commit.

---
+++ config/secrets/dev/dev.list.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'APP_ENV' => null,
+    'APP_SECRET' => null,
+    'CORS_ALLOW_ORIGIN' => null,
+    'DATABASE_URL' => null,
+    'JWT_PASSPHRASE' => null,
+    'JWT_PUBLIC_KEY' => null,
+    'JWT_SECRET_KEY' => null,
+    'SESSION_ENCRYPTION_KEY' => null,
+];
